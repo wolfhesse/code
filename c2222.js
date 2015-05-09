44de20f9694ef316fc5ba112d3cef8725422d447
wolfhesse <wolfgang.schuessel@gmail.com>
2015-05-09 12:13:10 +0200

    wp

--- c2222.js
+++ c2222.js
@@ -1,13 +1,5 @@
-// var SockJS = require('sockjs-client-node');
-// console.log(SockJS);
-// var sock = new SockJS('https://firehose.blippex.org');
-// sock.onmessage = function(e) {
-    // var data=JSON.parse(e.data);
-    // console.log(data.url);
-// };
 var id=Math.random();
-//var socket = require('socket.io-client')('http://0.0.0.0/');
-var socket = require('socket.io-client')('http://os23.type1.tv:2222/');
+var socket = require('socket.io-client')('http://metaloi.wolfspool.chickenkiller.com:2222/');
 socket.on('connect', function(){
 	console.log('client: connect '+id);
 	socket.emit('hello','client');
@@ -25,16 +17,3 @@ socket.on('disconnect', function(){
 	console.log('client: disconnect');
 	});
 
-// var client = require('socket.io-client');
-// console.log(client);
-// var conn = client.connect('https://firehose.blippex.org');
-// conn.on('connection',function(bus){
-// 
-	// bus.on('data', function(data){
-		// var d = JSON.parse(data);
-		// console.log(d.url);
-	// });
-// });
-// conn.on('data', function(conndata){
-// console.log('conndata');
-	// });
