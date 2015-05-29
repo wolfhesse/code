74b6921fb397500bfcc621843abfdfbcd00e069b
wolfhesse <wolfgang.schuessel@gmail.com>
2015-05-29 22:21:04 +0200

    wp

--- c2222.js
+++ c2222.js
@@ -2,16 +2,16 @@ var id=Math.random();
 var socket = require('socket.io-client')('http://metaloi.wolfspool.chickenkiller.com:2222/');
 socket.on('connect', function(){
 	console.log('client: connect '+id);
-	socket.emit('hello','client');
+	socket.emit('helo','client:'+id);
 	});
 socket.on('tick', function(data) {
 	console.log('client:'+id+' tick '+ data);
 	});
 socket.on('data', function(data) {
-	console.log('client:'+id+' data '+ data);
+	console.log('client:'+id+' data '+ JSON.stringify(data));
 	});
 socket.on('event', function(data){
-	console.log('client:'+id+' event '+ data);
+	console.log('client:'+id+' event '+ JSON.stringify(data));
 	});
 socket.on('disconnect', function(){
 	console.log('client: disconnect');
--- s2222.js
+++ s2222.js
@@ -3,27 +3,29 @@ var io = require('socket.io')();
 var last_s;
 io.listen(2222);
 io.on('connection', function (socket) {
-    last_s = socket;
-    if (null == ticker_interval) {
-        ticker_interval = setInterval(function () {
-                io.emit('tick', new Date);
-                // last client gets sync-start
-                last_s.emit('event', {'srv:last_s:tick' : new Date});
-                console.log('server: tick');
-            },
-            1000);
-    }
-    socket.on('helo', function (data) {
-        vals = JSON.stringify(data);
-        console.log('server: helo data ' + vals);
-        io.emit('data', {'srv:clt:data' : data});
-    });
-    socket.on('disconnect', function (socket) {
-        io.emit('event', 'another client disconnected');
-        console.log('server: disconnect');
-    });
+	last_s = socket;
+	if (null == ticker_interval) {
+		ticker_interval = setInterval(function () {
+			io.emit('tick', new Date);
+			// last client gets sync-start
+			last_s.emit('event', {'srv:last_s:tick' : new Date});
+			console.log('server: tick');
+		},
+		1000);
+	}
+	socket.on('helo', function (data) {
+		console.log('socket.helo: pre parse');
+		vals = JSON.stringify(data);
+		console.log('server: helo data ' + vals);
+		io.emit('data', {'srv:clt:data' : data});
+	});
+	socket.on('disconnect', function (socket) {
+		io.emit('event', 'another client disconnected');
+		console.log('server: disconnect');
+	});
 });
 
 setInterval(function () {
-    console.log('server: ticker_interval @ ' + new Date);
+	console.log('server: ticker_interval @ ' + new Date);
 }, 5000);
+
