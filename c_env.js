e7e7a2dca2e3b3a1580802c0bd873ccd81e77b88
rogera <wolfgang.schuessel@gmail.com>
2015-09-22 18:07:19 +0200

    connect-c9  prevent multiple event registration in client / reconnect-scenario

--- c_env.js
+++ c_env.js
@@ -10,20 +10,24 @@ var socket = require('socket.io-client')(server_address);
 socket.on('connect', function () {
     console.log('client: connect ' + id);
     socket.emit('helo', 'client:' + id);
+});
 
-    socket.on('tick', function (data) {
-        console.log('client:' + id + ' tick ' + data);
-    });
-    socket.on('data', function (data) {
-        console.log('client:' + id + ' data ' + JSON.stringify(data));
-    });
-    socket.on('event', function (data) {
-        console.log('client:' + id + ' event ' + JSON.stringify(data));
-    });
-    socket.on('disconnect', function () {
-        console.log('client: disconnect');
-    });
+// reconnect scenario: do not register event handlers multiple times..
 
+socket.on('tick', function (data) {
+    console.log('client:' + id + ' tick ' + data);
+});
+
+socket.on('data', function (data) {
+    console.log('client:' + id + ' data ' + JSON.stringify(data));
+});
+
+socket.on('event', function (data) {
+    console.log('client:' + id + ' event ' + JSON.stringify(data));
+});
+
+socket.on('disconnect', function () {
+    console.log('client: disconnect');
 });
 
 
